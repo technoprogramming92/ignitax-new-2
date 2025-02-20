4052733e7fbede408d47af79de78d7b7d1a8caf5
Saumya Rawal <114496132+technoprogramming92@users.noreply.github.com>
2025-02-20 15:43:52 +0530

    servicePage started - saumya 15:43 200225

---
+++ app/components/ServicesSection.jsx
@@ -0,0 +1,326 @@
+import React from "react";
+
+function ServicesSection() {
+  return (
+    <section className="service-area-three section-padding-top section-border-black">
+      <div className="container custom-container custom-container-semi-large">
+        <div className="row align-items-center gy-lg-5 gy-4">
+          <div className="col-lg-12 pe-lg-0">
+            <div className="section-title-three-sub">
+              <span className="d-inline-block position-relative fnt-weight-500 text_black fade_left">
+                OUR SERVICES
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12 service-three-pin-wrap overflow-hidden section-padding-bottom">
+            <div className="row gy-5 pin-section-area">
+              <div className="col-lg-5">
+                <div className="service-three-title-wrap">
+                  <div className="section-title-two">
+                    <h2 className="h2 fade_right">
+                      We drive value in complex problems by creating design
+                    </h2>
+                  </div>
+                  <a
+                    className="common-design-btn only-border mt-4 btn-trigger btn-bounce"
+                    href="service.html"
+                  >
+                    <span className="btn-flip">
+                      <span data-text="Explore All" className="!font-extrabold">
+                        Explore All
+                      </span>
+                    </span>
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-7 pe-lg-0">
+                <div className="service-three-box-wrap position-relative work-process-count">
+                  <div className="service-three-box">
+                    <h2 className="service-three-title position-relative d-inline-block reveal_anim-2 !font-bold">
+                      UI/UX DESIGN
+                    </h2>
+                    <ul className="service-three-list d-flex flex-wrap mt-3 list-disc">
+                      <li>Web Design</li>
+                      <li>App Design</li>
+                      <li>Prototyping</li>
+                      <li>Wireframing</li>
+                    </ul>
+                    <a
+                      className="common-design-btn-two mt-5 !font-extrabold no-underline"
+                      href="service.html"
+                    >
+                      Read More
+                      <div className="button-icon-wrap">
+                        <svg
+                          className="button-icon button-icon-one"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M13.4674 11.468L13.4674 6.51554L8.51498 6.51554"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M6.53246 13.4507L13.3982 6.58496"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                        <svg
+                          className="button-icon button-icon-two"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M13.4674 11.468L13.4674 6.51554L8.51498 6.51554"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M6.53246 13.4507L13.3982 6.58496"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="service-three-box">
+                    <h2 className="service-three-title position-relative d-inline-block reveal_anim-2">
+                      MOTION DESIGN
+                    </h2>
+                    <ul className="service-three-list d-flex flex-wrap mt-3 list-disc">
+                      <li>Web Design</li>
+                      <li>App Design</li>
+                      <li>Prototyping</li>
+                      <li>Wireframing</li>
+                    </ul>
+                    <a
+                      className="common-design-btn-two mt-5 !font-extrabold no-underline"
+                      href="service.html"
+                    >
+                      Read More
+                      <div className="button-icon-wrap">
+                        <svg
+                          className="button-icon button-icon-one"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M13.4674 11.468L13.4674 6.51554L8.51498 6.51554"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M6.53246 13.4507L13.3982 6.58496"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                        <svg
+                          className="button-icon button-icon-two"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M13.4674 11.468L13.4674 6.51554L8.51498 6.51554"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M6.53246 13.4507L13.3982 6.58496"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="service-three-box">
+                    <h2 className="service-three-title position-relative d-inline-block reveal_anim-2">
+                      3D DESIGN
+                    </h2>
+                    <ul className="service-three-list d-flex flex-wrap mt-3 list-disc">
+                      <li>Web Design</li>
+                      <li>App Design</li>
+                      <li>Prototyping</li>
+                      <li>Wireframing</li>
+                    </ul>
+                    <a
+                      className="common-design-btn-two mt-5 !font-extrabold no-underline"
+                      href="service.html"
+                    >
+                      Read More
+                      <div className="button-icon-wrap">
+                        <svg
+                          className="button-icon button-icon-one"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M13.4674 11.468L13.4674 6.51554L8.51498 6.51554"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M6.53246 13.4507L13.3982 6.58496"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                        <svg
+                          className="button-icon button-icon-two"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M13.4674 11.468L13.4674 6.51554L8.51498 6.51554"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M6.53246 13.4507L13.3982 6.58496"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="service-three-box">
+                    <h2 className="service-three-title position-relative d-inline-block reveal_anim-2">
+                      GRAPHIC DESIGN
+                    </h2>
+                    <ul className="service-three-list d-flex flex-wrap mt-3 list-disc">
+                      <li>Web Design</li>
+                      <li>App Design</li>
+                      <li>Prototyping</li>
+                      <li>Wireframing</li>
+                    </ul>
+                    <a
+                      className="common-design-btn-two mt-5 !font-extrabold no-underline"
+                      href="service.html"
+                    >
+                      Read More
+                      <div className="button-icon-wrap">
+                        <svg
+                          className="button-icon button-icon-one"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M13.4674 11.468L13.4674 6.51554L8.51498 6.51554"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M6.53246 13.4507L13.3982 6.58496"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                        <svg
+                          className="button-icon button-icon-two"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M13.4674 11.468L13.4674 6.51554L8.51498 6.51554"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M6.53246 13.4507L13.3982 6.58496"
+                            stroke="#0C0C25"
+                            strokeWidth="1.5"
+                            strokeMiterlimit="10"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ServicesSection;
---
+++ app/services/page.jsx
@@ -0,0 +1,25 @@
+import Footer from "../components/Footer";
+import ServicesSection from "../components/ServicesSection";
+export default function ServicesPage() {
+  return (
+    <>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <div className="cursor d-none d-lg-block"></div>
+          <ServicesSection />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
